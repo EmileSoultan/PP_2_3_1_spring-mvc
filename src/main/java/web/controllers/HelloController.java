042908d39bff9e541c7ac61a8c8e181f69590a78
Emile <emabish42@gmail.com>
2023-04-26 11:55:38 +0200

    Небольшие правки

---
+++ src/main/java/web/controllers/HelloController.java
@@ -0,0 +1,21 @@
+package web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HelloController {
+    @GetMapping(value = "/")
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC application");
+        messages.add("6.0.8 version by apr'23 ");
+        model.addAttribute("messages", messages);
+        return "index";
+    }
+}
